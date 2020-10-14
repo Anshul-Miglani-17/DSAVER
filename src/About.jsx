8f085d2f00944bbd95c1ae118299126e3ca971c7
Your Name <migs.anshul@gmail.com>
2020-10-15 01:19:37 +0530

    ag 15oct

--- src/About.jsx
+++ src/About.jsx
@@ -14,9 +14,7 @@ const About = () => {
               <div className="col-md-6 pb-5 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
                 <h1>Learn And Grow With <strong className="brand-name">DSAVER</strong></h1>
                 <h2 className="my-3">
-                <p>DSAVER is a user interactive website in which we take input from users and visualises the selected algorithm according to their choice with their input in it.</p>
-                <p><strong className="objective">The objective of this website is :-</strong> <br/>
-When we have started learning data structure and sorting algorithms we find it very difficult to understand on our own. So, the objective of this project is to provide students a platform to learn and understand complex algorithms in easy and attractive manner.</p>
+                <p>DSAVER stands for Data Structure and Algorithm Visualizer.<br/>It is a learning platform for Data Structures and Algorithms.<br/>When we have started learning data structure and sorting algorithms we find it very difficult to understand on our own. Therefore, we decided to do this project. As, our objective for this project is to provide students a platform to learn and understand complex algorithms in easy and attractive manner.</p>
                 </h2>
                 <div className="mt-3">
                   <NavLink to="/contact" className="btn-get-started ">
